9886b8cef1870d17bd579f201344604e67e1ce46
G.Ambatte <sean@robertson-family.nz>
2022-05-24 18:28:31 +1200

    Initial Tokenizer pass

--- src/index.js
+++ src/index.js
@@ -1,23 +1,34 @@
-export default function(options = {}) {
-  // extension code here
-
+export default function(options = {
+  enable=true
+}) {
+  if(!enable) {
+    return;
+  };
+  
   return {
+    lexer: {
+      constructor(options) {
+        this.tokens.vars = Object.create(null);
+      }
+    },
     tokenizer: {
-      paragraph(src) {
-        if (src !== 'example markdown') {
-          return false;
+      inline(src, vars) {
+        // Define variables:
+        // [key]<<[value]
+        const varDefine = /^\[(\w+)\]:{2}\[(.+)\]/;
+        // Find variables in text:
+        // >>[key]
+        const varSearch = />{2}\[(\w+)\]/;
+        
+        if (const cap = varDefine.exec(src)) {
+          // console.log(cap[1]);
+          // console.log(cap[2]);
+          let key = cap[1].replace(/\s+/g, ' ');
+          let value = cap[2].replace(/\s+/g, ' ');
+          vars[key] = value;
         }
-
-        const token = {
-          type: 'paragraph',
-          raw: src,
-          text: 'example html',
-          tokens: []
-        };
-
-        this.lexer.inline(token.text, token.tokens);
-
-        return token;
+        
+        return;
       }
     }
   };
