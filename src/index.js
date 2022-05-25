832e4c50493475a241edf3777fd26e4fad8ed9bd
G.Ambatte <sean@robertson-family.nz>
2022-05-25 21:52:36 +1200

    Initial functionality pass

--- src/index.js
+++ src/index.js
@@ -1,35 +1,74 @@
-export default function(options = {
-  enable=true
-}) {
-  if(!enable) {
-    return;
-  };
-  
+export default function(options = {}) {
+  if (!options.enable) return false;
+  const variables = [];
   return {
-    lexer: {
-      constructor(options) {
-        this.tokens.vars = Object.create(null);
-      }
-    },
-    tokenizer: {
-      inline(src, vars) {
-        // Define variables:
-        // [key]<<[value]
-        const varDefine = /^\[(\w+)\]:{2}\[(.+)\]/;
-        // Find variables in text:
-        // >>[key]
-        const varSearch = />{2}\[(\w+)\]/;
-        
-        if (const cap = varDefine.exec(src)) {
-          // console.log(cap[1]);
-          // console.log(cap[2]);
-          let key = cap[1].replace(/\s+/g, ' ');
-          let value = cap[2].replace(/\s+/g, ' ');
-          vars[key] = value;
+    extensions: [
+      {
+        name: 'variableDefinition',
+        level: 'inline', // Is this a block-level or inline-level tokenizer?
+        start(src) { return src.index; }, // Hint to Marked.js to stop and check for a match
+        tokenizer(src, tokens) {
+          const variableDefinition = /^\[(\w+)\]<{2}\[([^\n\]]+)\]\n/;
+          const capDef = variableDefinition.exec(src);
+
+          if (capDef) {
+            const item = {
+              type: 'variableDefinition',
+              raw: capDef[0],
+              key: capDef[1],
+              value: capDef[2],
+              tokens: []
+            };
+            this.lexer.inline(item.value, item.tokens);
+            variables.push(item);
+            return item;
+          }
+        },
+
+        renderer(token) {
+          if (token.type === 'variableDefinition') {
+            return null;
+          }
         }
-        
-        return;
+      },
+
+      {
+        name: 'variableCall',
+        level: 'inline', // Is this a block-level or inline-level tokenizer?
+        start(src) { return src.index; }, // Hint to Marked.js to stop and check for a match
+        tokenizer(src, tokens) {
+          const variableCall = /^\[([^\n\]]+)\]>{2} */;
+          const capCall = variableCall.exec(src);
+
+          if (capCall) {
+            const item = {
+              type: 'variableCall',
+              raw: capCall[0],
+              key: capCall[1],
+              tokens: []
+            };
+            this.lexer.inline(item.key, item.tokens);
+            return item;
+          }
+        },
+
+        renderer(token) {
+          if (token.type === 'variableCall') {
+            // const output = this.parser.parseInline(variables.find((item) => {
+            //   return item.key === token.key;
+            // })?.tokens || token.tokens);
+
+            const output = variables.find((item) => {
+              return item.key === token.key;
+            });
+            if (output) {
+              return this.parser.parseInline(output.tokens);
+            }
+            return `[${this.parser.parseInline(token.tokens)}]>>`;
+            // return output;
+          }
+        }
       }
-    }
+    ]
   };
 }
